610ca9a178163ee33961e76c79a900a2d9eb9b1f
YukinoAkira <84516117+KawaiiGpp@users.noreply.github.com>
2026-07-06 19:31:00 +0800

    feat(sys): lock app orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:weather_beta/app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  _setupPreferredOrientations();
+
   runApp(const App());
+}
+
+void _setupPreferredOrientations() {
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 }
