91e34c7a9222c637f1fb3c074b53142614259bb6
YukinoAkira <84516117+KawaiiGpp@users.noreply.github.com>
2026-07-06 19:35:07 +0800

    feat(sys): setup navigation bar style for android

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:weather_beta/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  _setupNavigationBarStyle();
   _setupPreferredOrientations();
 
   runApp(const App());
@@ -12,4 +15,19 @@ void main() async {
 void _setupPreferredOrientations() {
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+}
+
+Future<void> _setupNavigationBarStyle() async {
+  if (kIsWeb) return;
+  if (defaultTargetPlatform != TargetPlatform.android) return;
+  
+  await WidgetsBinding.instance.endOfFrame;
+
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      systemNavigationBarColor: Colors.transparent,
+      systemNavigationBarIconBrightness: Brightness.dark,
+      systemNavigationBarContrastEnforced: false,
+    ),
+  );
 }
