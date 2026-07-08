3fadcee2c168f564fa08e49ce177a91ec310c159
YukinoAkira <84516117+KawaiiGpp@users.noreply.github.com>
2026-07-08 11:27:08 +0800

    chore: setup dot-env support

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:weather_beta/app.dart';
 
 void main() async {
@@ -9,6 +10,7 @@ void main() async {
   _setupNavigationBarStyle();
   _setupPreferredOrientations();
 
+  await dotenv.load(fileName: ".env");
   runApp(const App());
 }
 
