b05b7065f9fe25d76472e7ffd69a33a9fbfe7909
YukinoAkira <84516117+KawaiiGpp@users.noreply.github.com>
2026-07-20 16:59:54 +0800

    feat(common): simplify range

--- lib/core/common/range.dart
+++ lib/core/common/range.dart
@@ -1,22 +1,8 @@
-class Range extends _ValueRange<double> {
-  const Range(super.min, super.max);
+class Range {
+  final double min;
+  final double max;
 
-  @override
   double get size => max - min;
-}
 
-class IntRange extends _ValueRange<int> {
-  const IntRange(super.min, super.max);
-
-  @override
-  int get size => max - min;
-}
-
-class _ValueRange<T extends num> {
-  final T min;
-  final T max;
-
-  num get size => max - min;
-
-  const _ValueRange(this.min, this.max) : assert(min <= max);
+  const Range(this.min, this.max);
 }
