de0f78d99cbcc34cb71c6c952137fa88380158c2
YukinoAkira <84516117+KawaiiGpp@users.noreply.github.com>
2026-08-06 20:43:43 +0800

    feat(extensions): add expandToList to IterableExtension

--- lib/core/extensions/iterable_extension.dart
+++ lib/core/extensions/iterable_extension.dart
@@ -2,4 +2,8 @@ extension IterableExtension<T> on Iterable<T> {
   List<R> mapToList<R>(R Function(T it) transform) {
     return map(transform).toList();
   }
+
+  List<R> expandToList<R>(Iterable<R> Function(T it) transform) {
+    return expand(transform).toList();
+  }
 }
